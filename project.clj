62a930ad56d71b24bf7cda51cf47343c56ddc017
Robert Boyd <robert.boyd@me.com>
2013-02-01 21:44:30 -0600

    Adding compojure.

--- project.clj
+++ project.clj
@@ -4,8 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1450"]]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+                 [org.clojure/clojurescript "0.0-1450"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]
+            [lein-cljsbuild "0.3.0"]]
+  :source-paths ["src/clj"
+                 "src/cljs"]
+  :ring {:handler amnesiac.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}}
   :cljsbuild
   {:builds
    [{:source-paths ["src/cljs"],
