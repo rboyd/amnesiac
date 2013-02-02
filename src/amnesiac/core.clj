49af8e5f29e190c9f88b4777c2d9d51f66f2c81d
Robert Boyd <robert.boyd@me.com>
2013-02-01 20:00:10 -0600

    Initial commit.

---
+++ src/amnesiac/core.clj
@@ -0,0 +1,6 @@
+(ns amnesiac.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/amnesiac/core_test.clj
@@ -0,0 +1,7 @@
+(ns amnesiac.core-test
+  (:use clojure.test
+        amnesiac.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
