626464361feeb80fefe1620960888fec95f6adf7
Robert Boyd <robert.boyd@me.com>
2013-02-01 22:34:23 -0600

    Adding horribad design placeholder.

--- src/clj/amnesiac/handler.clj
+++ src/clj/amnesiac/handler.clj
@@ -1,10 +1,14 @@
 (ns amnesiac.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :as resp]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
+  (GET "/stylesheets/main.css" [] (resp/resource-response "stylesheets/main.css" {:root "public"}))
+  (GET "/images/prev.png" [] (resp/resource-response "/images/prev.png" {:root "public"}))
+  (GET "/images/next.png" [] (resp/resource-response "/images/next.png" {:root "public"}))
   (route/not-found "Not Found"))
 
 (def app
