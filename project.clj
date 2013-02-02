a96925b4383515075a22881f5900008aa0e27fba
Robert Boyd <robert.boyd@me.com>
2013-02-01 20:12:19 -0600

    Adding ClojureScript.

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
 (defproject amnesiac "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Amnesiac: Spaced repetition software"
+  :url "http://github.com/rboyd/amnesiac"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojurescript "0.0-1450"]]
+  :plugins [[lein-cljsbuild "0.3.0"]]
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src/cljs"],
+     :compiler
+     {:pretty-print true,
+      :output-to "resources/public/amnesiac.js",
+      :optimizations :whitespace}}]})
---
+++ src/cljs/amnesiac.cljs
@@ -0,0 +1,6 @@
+(ns amnesiac.core)
+
+(defn main
+  "The program's entrypoint"
+  []
+  (js/alert "amnesiac"))
