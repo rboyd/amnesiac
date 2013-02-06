6207c68a4d6668d3f59af065bd61d89e49faf2cf
Robert Boyd <robert.boyd@me.com>
2013-02-05 21:35:44 -0600

    Cleaning up routing with compojure.route/resources. Adding background texture.

--- src/clj/amnesiac/handler.clj
+++ src/clj/amnesiac/handler.clj
@@ -7,15 +7,13 @@
 
 (defroutes app-routes
   (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
-  (GET "/stylesheets/main.css" [] (resp/resource-response "stylesheets/main.css" {:root "public"}))
-  (GET "/amnesiac.js" [] (resp/resource-response "amnesiac.js" {:root "public"}))
-  (GET "/images/prev.png" [] (resp/resource-response "/images/prev.png" {:root "public"}))
-  (GET "/images/next.png" [] (resp/resource-response "/images/next.png" {:root "public"}))
   (GET "/cards/random" [] (resp/response (db/random-card)))
   (POST "/cards" request {:status 200 :body (->
                                              (:body request)
                                              slurp
                                              db/save-card
-                                             str)}))
+                                             str)})
+  (route/resources "/"))
+
 (def app
   (handler/site app-routes))
